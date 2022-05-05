02d37e55d8ae7e46d9146067d4402d047c660727
Agata <amomot@bio-prodict.nl>
2022-05-05 13:54:17 +0200

    Declared forwarded and initial nodes

--- src/phone_forward.c
+++ src/phone_forward.c
@@ -4,11 +4,28 @@
 #include "phone_forward.h"
 #include <stdint.h>
 
-typedef struct InitialNode {
-    struct InitialNode* alphabet[10];
-    uint16_t depth;
-    uint8_t terminalForwarded;
-} InitialNode;
-typedef struct PhoneForward {
+struct ForwardedNode;
 
+typedef struct InitialNode {
+    struct InitialNode* ancestor;
+    struct ForwardedNode* forward;
+    struct InitialNode* alphabet[10];
+    uint64_t depth;
+    uint8_t terminalForwarded;
+    uint64_t indexForward;
+    uint64_t howManyHavePassedThrough;
+} InitialNode;
+
+typedef struct ForwardedNode {
+    struct ForwardedNode* alphabet[10];
+    uint8_t terminalForwarding;
+    uint64_t numForwardedNodes;
+    InitialNode** forwardedNodes;
+    char* forwardedPrefix;
+    uint64_t howManyHavePassedThrough;
+} ForwardedNode;
+
+typedef struct PhoneForward {
+    ForwardedNode* forwardedRoot;
+    InitialNode* initialRoot;
 } PhoneForward;
